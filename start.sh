afb9c9a3134fc3b9d07190f9a767700d740aed8d
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2023-01-13 22:46:29 +0100

    Do not start qemu if compiling the project fail

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # This script is used to build and run the project
 
-cargo build --target x86_64-unknown-uefi
+cargo build --target x86_64-unknown-uefi || exit 1
 mkdir -p esp/efi/boot
 cp target/x86_64-unknown-uefi/debug/uefi-morpion.efi esp/efi/boot/bootx64.efi
 qemu-system-x86_64 -enable-kvm -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE.fd -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_VARS.fd -drive format=raw,file=fat:rw:esp
